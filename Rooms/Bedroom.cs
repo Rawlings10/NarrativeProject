78ed2edca4778326a6805e01ccfbe964ccb382a7
Rawlings_10 <rawllingsoduwa@gmail.com>
2024-03-19 13:40:38 -0400

    Add my Name

--- Rooms/Bedroom.cs
+++ Rooms/Bedroom.cs
@@ -10,6 +10,7 @@ namespace NarrativeProject.Rooms
 The [door] in front of you leads to your living room.
 Your private [bathroom] is to your left.
 From your closet, you see the [attic].
+To your right the door leads to the [kitchen].
 ";
 
         internal override void ReceiveChoice(string choice)
@@ -34,6 +35,9 @@ From your closet, you see the [attic].
                 case "attic":
                     Console.WriteLine("You go up and enter your attic.");
                     Game.Transition<AtticRoom>();
+                    break;
+                case "kitchen":
+                    Console.WriteLine("You drink a cup of coffee while in the kitchen");
                     break;
                 default:
                     Console.WriteLine("Invalid command.");
